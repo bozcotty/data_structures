cd979c8ddddc402199737dceaea7c7e97f9d3330
Scott Hale <shalemont@gmail.com>
2014-10-15 13:53:31 -0700

    complete insertion sort with test, benchmark

---
+++ lib/insert_sort.rb
@@ -0,0 +1,21 @@
+class Array
+  def insert_sort(array)
+    (1..array.length-1).each do |index|
+      back = index
+      while back > 0
+        if array[back-1] > array[back]
+          #swap
+          array[back-1], array[back] = array[back], array[back-1]
+          back = back - 1
+        else
+          back=0
+        end
+      end
+    end
+    p array
+  end
+end
+
+# insert_sort([4,51,3,8,6,2,9,10,12,18])
+
+
