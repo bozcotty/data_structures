cd40d46e8a41fa69676cf6731e185d2985c2be71
Scott Hale <shalemont@gmail.com>
2014-10-25 15:56:36 -0700

    bin tree complete logic

--- lib/binary_tree.rb
+++ lib/binary_tree.rb
@@ -1,14 +1,13 @@
 class Node
-  attr_accessor :left, :right, :val
-
   def initialize()
     @left = nil
     @right = nil
     @value = nil
   end
+
+  attr_accessor :left, :right, :value
 end
 
-# require 'Node'
 
 class BinaryTree
   attr_accessor :root
@@ -26,3 +25,82 @@ class BinaryTree
       pre_order(node.right)
     end
   end
+
+  def in_order(node)
+    if node.left != nil
+      in_order(node.left)
+    end
+    p node.value
+    if node.right != nil
+      in_order(node.right)
+    end
+  end
+
+  def post_order(node)
+    if node.left != nil
+      post_order(node.left)
+    end
+    if node.right != nil
+      post_order(node.right)
+    end
+    p node.value
+  end
+
+  def traversal
+    print "Pre-Order Traversal:\n"
+    pre_order(@root)
+    print "\n"
+
+    print "In-Order Traversal:\n"
+    in_order(@root)
+    print "\n"
+
+    print "Post-Order Traversal:\n"
+    post_order(@root)
+  end
+end
+
+bintree = BinaryTree.new
+root = bintree.root
+root.value = "Tim"
+
+node = Node.new
+node.value = "Jony"
+root.left = node
+
+node = Node.new
+node.value = "Dan"
+root.left.left = node
+
+node = Node.new
+node.value = "Katie"
+root.left.right = node
+
+node = Node.new
+node.value = "Peter"
+root.left.right.left = node
+
+node = Node.new
+node.value = "Andrea"
+root.left.right.right = node
+
+node = Node.new
+node.value = "Phil"
+root.right = node
+
+node = Node.new
+node.value = "Craig"
+root.right.left = node
+
+node = Node.new
+node.value = "Eddie"
+root.right.right = node
+
+bintree.traversal
+
+
+
+
+
+
+
